7514d6c16293033ccba417eba2c6f789084dca56
cvbnt <5465497@gmail.com>
2018-04-13 20:03:08 +0800

    2018-04-13

--- app/src/main/java/t/n/b/v/c/music/Play.java
+++ app/src/main/java/t/n/b/v/c/music/Play.java
@@ -2,14 +2,17 @@ package t.n.b.v.c.music;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.Random;
 
 import static java.lang.StrictMath.abs;
 
@@ -25,20 +28,27 @@ public class Play extends AppCompatActivity {
     private ImageButton mPrevious;
     private ImageButton mPlay;
     private ImageButton mNext;
+    private ImageButton mShuffle;
+    private ImageButton mRepeat;
+    private Drawable drawable;
     public TextView mNowTime;
     public TextView mDurationTime;
     public MarqueeTextView mName;
     public MarqueeTextView mSinger;
     private ImageButton mImageButton;
     private android.support.v7.widget.Toolbar mToolBar;
-    private MediaPlayer mMediaPlayer=new MediaPlayer();
+    private MediaPlayer mMediaPlayer;
     private static int DRAWABLE_STATE=0;
+    private static int SHUFFLE_STATE=0;
+    private static int REPEAT_STATE=0;
     private Handler mHandler;
     private Context context;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music_play);
+        mMediaPlayer=new MediaPlayer();
+        mMediaPlayer.setOnCompletionListener(mediaPlayer -> checkState());
         context=Play.this;
         m_size=Scan.getMusicData(context).size();
         Intent intent=getIntent();
@@ -46,15 +56,21 @@ public class Play extends AppCompatActivity {
         fvbi();
         refresh();
         initMediaPlayer(m_Path);
-        m_size=Scan.getMusicData(context).size();
-        mSeekBar.setMax(mMediaPlayer.getDuration());
+        drawable=mShuffle.getDrawable();
         setSupportActionBar(mToolBar);
-        mImageButton.setOnClickListener(new View.OnClickListener() {
+        mHandler=new Handler();
+        runOnUiThread(new Runnable() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Play.this,MainActivity.class));
+            public void run() {
+                if(mMediaPlayer != null){
+                    int mCurrentPosition = mMediaPlayer.getCurrentPosition();
+                    mSeekBar.setProgress(mCurrentPosition);
+                    mNowTime.setText(Scan.formatTime(mCurrentPosition));
+                }
+                mHandler.postDelayed(this, 1000);
             }
         });
+        mImageButton.setOnClickListener(view -> startActivity(new Intent(Play.this,MainActivity.class)));
         mPlay.setOnClickListener(view -> {
             if (DRAWABLE_STATE==0){
                 mPlay.setImageDrawable(getDrawable(R.drawable.ic_pause));
@@ -70,40 +86,32 @@ public class Play extends AppCompatActivity {
                 }
             }
         });
-        mPrevious.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mGetPosition = mGetPosition-1;
-                if (mGetPosition<0){
-                    mGetPosition=m_size-(abs(mGetPosition)%m_size);
-                }
-                refresh();
-                initMediaPlayer(m_Path);
-                mSeekBar.setMax(mMediaPlayer.getDuration());
+        mPrevious.setOnClickListener(view -> playPreviousSong());
+        mNext.setOnClickListener(view -> playNextSong());
+        mRepeat.setOnClickListener(view -> {
+            if (REPEAT_STATE==0){
+                REPEAT_STATE=1;
+                SHUFFLE_STATE=0;
+                mRepeat.setImageDrawable(getDrawable(R.drawable.ic_repeat_one));
+                drawable.setTint(Color.BLACK);
+            }else {
+                REPEAT_STATE=0;
+                SHUFFLE_STATE=0;
+                mRepeat.setImageDrawable(getDrawable(R.drawable.ic_repeat));
+                drawable.setTint(Color.BLACK);
             }
         });
-        mNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mGetPosition = mGetPosition+1;
-                if (mGetPosition>=0){
-                    mGetPosition=abs(mGetPosition)%m_size;
-                }
-                refresh();
-                initMediaPlayer(m_Path);
-                mSeekBar.setMax(mMediaPlayer.getDuration());
-            }
-        });
-        mHandler=new Handler();
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if(mMediaPlayer != null){
-                    int mCurrentPosition = mMediaPlayer.getCurrentPosition();
-                    mSeekBar.setProgress(mCurrentPosition);
-                    mNowTime.setText(Scan.formatTime(mCurrentPosition));
-                }
-                mHandler.postDelayed(this, 1000);
+        mShuffle.setOnClickListener(view -> {
+            if (SHUFFLE_STATE==0) {
+                SHUFFLE_STATE = 1;
+                REPEAT_STATE = 0;
+                mRepeat.setImageDrawable(getDrawable(R.drawable.ic_repeat));
+                drawable.setTint(Color.parseColor("#FF4081"));
+            }else {
+                SHUFFLE_STATE=0;
+                REPEAT_STATE=0;
+                mRepeat.setImageDrawable(getDrawable(R.drawable.ic_repeat));
+                drawable.setTint(Color.BLACK);
             }
         });
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -114,7 +122,7 @@ public class Play extends AppCompatActivity {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                mPlay.setImageDrawable(getDrawable(R.drawable.ic_pause));
             }
 
             @Override
@@ -145,28 +153,80 @@ public class Play extends AppCompatActivity {
         mName=findViewById(R.id.tv_name);
         mSinger=findViewById(R.id.tv_singer);
         mImageButton=findViewById(R.id.imageButton);
+        mShuffle=findViewById(R.id.shuffleButton);
+        mRepeat=findViewById(R.id.repeatButton);
     }
     private void initMediaPlayer(String string) {
         try{
             mMediaPlayer.setDataSource(string);                               //设置mediaPlayer
             mMediaPlayer.prepare();                                           //让mediaPlayer进入到准备状态
+            mSeekBar.setMax(mMediaPlayer.getDuration());
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+    private void playPreviousSong() {
+        mMediaPlayer.stop();
+        mMediaPlayer.reset();
+        mGetPosition = mGetPosition-1;
+        if (mGetPosition<0){
+            mGetPosition=m_size-(abs(mGetPosition)%m_size);
+        }
+        refresh();
+        initMediaPlayer(m_Path);
+        mMediaPlayer.start();
+    }
 
+    private void playNextSong() {
+        mMediaPlayer.stop();
+        mMediaPlayer.reset();
+        mGetPosition = mGetPosition+1;
+        if (mGetPosition>=0){
+            mGetPosition=abs(mGetPosition)%m_size;
+        }
+        refresh();
+        initMediaPlayer(m_Path);
+        mMediaPlayer.start();
+    }
+    private void checkState(){
+        if ((REPEAT_STATE==0)&&(SHUFFLE_STATE==0)){
+            playNextSong();
+        }else if ((REPEAT_STATE==1)&&(SHUFFLE_STATE==0)){
+            mMediaPlayer.start();
+        }else if ((REPEAT_STATE==0)&&(SHUFFLE_STATE==1)){
+            int mRandomPosition=new Random().nextInt(1000);
+            mRandomPosition=abs(mRandomPosition)%m_size;
+            if (mRandomPosition==mGetPosition){
+                mRandomPosition=mRandomPosition+1;
+            }
+            music=Scan.getMusicData(context).get(mRandomPosition);
+            m_Path=music.getPath();
+            m_Name=music.getName().replace(".mp3","");
+            m_Singer=music.getSinger();
+            m_Duration=Scan.formatTime(music.getDuration());
+            mName.setText(m_Name);
+            mSinger.setText(m_Singer);
+            mDurationTime.setText(m_Duration);
+            mMediaPlayer.reset();
+            initMediaPlayer(m_Path);
+            mMediaPlayer.start();
+        }
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (mMediaPlayer!=null){
+            mMediaPlayer.reset();
+            mMediaPlayer.stop();
+            mMediaPlayer.release(); //APP处于onDestroy时释放MediaPlayer的资源
+            mMediaPlayer=null;//回收资源
+        }
+        mHandler.removeCallbacksAndMessages(null);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mMediaPlayer!=null){
-            mMediaPlayer.stop();
-            mMediaPlayer.release(); //APP处于onDestroy时释放MediaPlayer的资源
-        }
-        mHandler.removeCallbacksAndMessages(null);
     }
+
 }
